19ae709aa42a2eaf0bcfbbeba413f8195cb6bc51
YaaMaa <shio.yama520@gmail.com>
2021-09-20 17:45:43 +0900

    Install webpack

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  target: "node",
+  entry: "./src/index.ts",
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+};
