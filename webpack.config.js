bea01fcc0dedaa45775fc124e26fe2191ea6b774
YaaMaa <shio.yama520@gmail.com>
2021-09-20 19:15:28 +0900

    Apply prettier

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,22 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  mode: "development",
-  target: "node",
-  entry: "./src/index.ts",
+  mode: 'development',
+  target: 'node',
+  entry: './src/index.ts',
   output: {
-    filename: "index.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
       {
         test: /\.ts$/,
-        use: "ts-loader",
+        use: 'ts-loader',
       },
     ],
   },
   resolve: {
-    extensions: [".ts", ".js"],
+    extensions: ['.ts', '.js'],
   },
 };
