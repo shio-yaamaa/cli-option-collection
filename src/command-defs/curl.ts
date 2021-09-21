7889f461f8158e666da5a3c587e5750d0220a5f5
YaaMaa <shio.yama520@gmail.com>
2021-09-21 22:18:10 +0900

    Add alternative sources for curl

--- src/command-defs/curl.ts
+++ src/command-defs/curl.ts
@@ -2,6 +2,9 @@ import { FetchFunction, Command } from '../types';
 
 // NOTE: Documentation for curl-config could not be found in the same domain
 
+// Alternative sources:
+// - https://github.com/curl/curl/tree/master/docs/cmdline-opts
+
 export const fetchCurl: FetchFunction = async (): Promise<Command[]> => {
   const url = 'https://curl.se/docs/manpage.html';
   return [];
