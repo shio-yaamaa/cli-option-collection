412bcba9daba6d3c5c0a2ee0f586431ce57c1be7
YaaMaa <shio.yama520@gmail.com>
2021-09-25 14:00:46 +0900

    Implement fetcher for mysqldump

--- src/command-defs/mysqldump.ts
+++ src/command-defs/mysqldump.ts
@@ -1,8 +1,12 @@
 import { FetchFunction, Command } from '../types';
 import { SourceDef, mysql } from '../common-fetchers/mysql';
 
+// BUG: "--all-tablespaces, -Y" is not fetched
+//      because this option is not specified in the option table.
+
 const sourceDef: SourceDef = {
-  url: 'https://dev.mysql.com/doc/refman/8.0/en/mysql-command-options.html',
+  commandName: 'mysqldump',
+  url: new URL('https://dev.mysql.com/doc/refman/8.0/en/mysqldump.html'),
 };
 
 export const fetchMysqldump: FetchFunction = async (): Promise<Command[]> =>
