3f7b6ca4d9221cd0f3f18043bb95640687abd145
YaaMaa <shio.yama520@gmail.com>
2021-09-26 03:12:03 +0900

    Add string utils

--- src/utils/forFetcher/string.test.ts
+++ src/utils/forFetcher/string.test.ts
@@ -1,8 +1,19 @@
-import { adjustSpacingAroundComma } from './string';
+import { adjustSpacingAroundComma, splitByMultipleDelimiters } from './string';
 
 describe('adjustSpacingAroundComma', () => {
   test('test', () => {
     expect(adjustSpacingAroundComma('abc , abc')).toBe('abc, abc');
     expect(adjustSpacingAroundComma('abc,abc')).toBe('abc, abc');
+  });
+});
+
+describe('splitByMultipleDelimiters', () => {
+  test('test', () => {
+    expect(splitByMultipleDelimiters('a, b and c', [',', 'and'])).toStrictEqual(
+      ['a', 'b', 'c']
+    );
+    expect(
+      splitByMultipleDelimiters('a, b, and c', [',', 'and'])
+    ).toStrictEqual(['a', 'b', 'c']);
   });
 });
--- src/utils/forFetcher/string.ts
+++ src/utils/forFetcher/string.ts
@@ -5,3 +5,22 @@ export const adjustSpacingAroundComma = (original: string): string => {
     .map((item) => item.trim())
     .join(', ');
 };
+
+export const splitByMultipleDelimiters = (
+  string: string,
+  delimiters: (string | RegExp)[]
+): string[] => {
+  let strings = [string];
+  for (const delimiter of delimiters) {
+    const newStrings = [];
+    for (const string of strings) {
+      const split = string
+        .split(delimiter)
+        .map((item) => item.trim())
+        .filter((item) => item.length > 0);
+      newStrings.push(...split);
+    }
+    strings = newStrings;
+  }
+  return strings;
+};
