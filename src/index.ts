80e7c6db8263da990c4ef3b6703c30b95bd320a1
YaaMaa <shio.yama520@gmail.com>
2021-09-27 00:41:59 +0900

    Specify multiple commands to fetch

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,13 @@ import { baseCommandToFetchFunction } from './commands';
 import { writeSnapshot } from './snapshot';
 
 const main = async () => {
-  const baseCommandName = 'mysqlslap';
+  const baseCommandNames = ['perl'];
+  for (const baseCommandName of baseCommandNames) {
+    await fetchOptions(baseCommandName);
+  }
+};
 
+const fetchOptions = async (baseCommandName: string) => {
   const fetchFunction = baseCommandToFetchFunction.get(baseCommandName);
   if (!fetchFunction) {
     console.log('No such base command');
