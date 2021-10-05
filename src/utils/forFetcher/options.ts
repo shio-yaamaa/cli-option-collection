5e0cd1d00755b4deccdc6b7ffbe1f400ab1082a9
YaaMaa <shio.yama520@gmail.com>
2021-10-06 01:51:45 +0900

    Use uniqueBy when picking unique options

--- src/utils/forFetcher/options.ts
+++ src/utils/forFetcher/options.ts
@@ -1,27 +1,5 @@
-import { Option, OptionType } from '../../types';
+import { Option } from '../../types';
+import { uniqueBy } from '../utils';
 
-export const uniqueOptions = (options: Option[]): Option[] => {
-  const uniqueOptions: Option[] = [];
-
-  const shortOptionMap = new Map<string, boolean>();
-  const longOptionMap = new Map<string, boolean>();
-
-  for (const option of options) {
-    switch (option.type) {
-      case OptionType.SHORT:
-        if (!shortOptionMap.has(option.key)) {
-          uniqueOptions.push(option);
-          shortOptionMap.set(option.key, true);
-        }
-        break;
-      case OptionType.LONG:
-        if (!longOptionMap.has(option.key)) {
-          uniqueOptions.push(option);
-          longOptionMap.set(option.key, true);
-        }
-        break;
-    }
-  }
-
-  return uniqueOptions;
-};
+export const uniqueOptions = (options: Option[]): Option[] =>
+  uniqueBy(options, (option) => [option.type, option.key]);
