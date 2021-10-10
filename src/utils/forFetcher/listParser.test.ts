e6f408b9ba6aa3386d35d179df94c75911b5f0f0
YaaMaa <shio.yama520@gmail.com>
2021-10-10 16:37:00 +0900

    Add test

--- src/utils/forFetcher/listParser.test.ts
+++ src/utils/forFetcher/listParser.test.ts
@@ -5,9 +5,13 @@ describe('parseTabbedTextList', () => {
     const text = `
       item1      Description of item1
       item2      Description of item2
-                 Description can be multi-line
+                 Description can be multi-line and can contain  double  spaces.
 
       long item  Description of long item
+      long long item  Description of long long item
+                      which has more indentation.
+        Something in between
+                      This is not description.
     `;
     const listItems = parseTabbedTextList(text);
     expect(listItems).toStrictEqual([
@@ -19,12 +23,19 @@ describe('parseTabbedTextList', () => {
         title: 'item2',
         descriptionLines: [
           'Description of item2',
-          'Description can be multi-line',
+          'Description can be multi-line and can contain  double  spaces.',
         ],
       },
       {
         title: 'long item',
         descriptionLines: ['Description of long item'],
+      },
+      {
+        title: 'long long item',
+        descriptionLines: [
+          'Description of long long item',
+          'which has more indentation.',
+        ],
       },
     ]);
   });
