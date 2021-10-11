c894f263ded3ff292d28332df9e6b8093a809cbf
YaaMaa <shio.yama520@gmail.com>
2021-10-11 15:13:52 +0900

    Override formatters

--- src/utils/dom.ts
+++ src/utils/dom.ts
@@ -3,5 +3,15 @@ import { htmlToText } from 'html-to-text';
 // An alternative to HTMLElement.innerText because JSDOM does not support it.
 // Use this instead of HTMLElement.textContent
 // when you want to convert an element to how browsers would render it.
-export const getInnerText = (element: Element) =>
-  htmlToText(element.outerHTML, { wordwrap: false });
+export const getInnerText = (element: Element) => {
+  const text = htmlToText(element.outerHTML, {
+    wordwrap: false,
+    selectors: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'].map((selector) => ({
+      selector,
+      options: {
+        uppercase: false,
+      },
+    })),
+  });
+  return text;
+};
