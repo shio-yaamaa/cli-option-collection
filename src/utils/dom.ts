c9363b2643c197f517f4a1ab416421f5bca6e87a
YaaMaa <shio.yama520@gmail.com>
2021-10-11 15:21:40 +0900

    Remove braces

--- src/utils/dom.ts
+++ src/utils/dom.ts
@@ -3,8 +3,8 @@ import { htmlToText } from 'html-to-text';
 // An alternative to HTMLElement.innerText because JSDOM does not support it.
 // Use this instead of HTMLElement.textContent
 // when you want to convert an element to how browsers would render it.
-export const getInnerText = (element: Element) => {
-  const text = htmlToText(element.outerHTML, {
+export const getInnerText = (element: Element) =>
+  htmlToText(element.outerHTML, {
     wordwrap: false,
     selectors: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'].map((selector) => ({
       selector,
@@ -13,5 +13,3 @@ export const getInnerText = (element: Element) => {
       },
     })),
   });
-  return text;
-};
