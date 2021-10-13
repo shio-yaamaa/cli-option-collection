2880418f344df03606524dd3581707f7d5556b1c
shio-yaamaa <shio.yama520@gmail.com>
2021-10-14 05:53:56 +0900

    Add notes

--- src/fetchers/systemd/index.ts
+++ src/fetchers/systemd/index.ts
@@ -1,5 +1,7 @@
 import { build } from './builder';
 
+// NOTE: There are more systemd-related commands that have not been collected.
+
 export const systemd = {
   journalctl: build('journalctl'),
   systemctl: build('systemctl'),
