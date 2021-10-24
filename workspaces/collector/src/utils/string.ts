cbfae151cf09d79862c09a75f19ecfb2b3cc4839
YaaMaa <shio.yama520@gmail.com>
2021-10-24 14:59:37 +0900

    Add collector workspace

--- workspaces/collector/src/utils/string.ts
+++ workspaces/collector/src/utils/string.ts
@@ -1,20 +1,5 @@
 import { isString } from './typeGuards';
 
-export const escapeHTML = (string: string): string => {
-  return string.replace(/[&'`"<>]/g, (match) => {
-    return (
-      {
-        '&': '&amp;',
-        "'": '&#x27;',
-        '`': '&#x60;',
-        '"': '&quot;',
-        '<': '&lt;',
-        '>': '&gt;',
-      }[match] || match
-    );
-  });
-};
-
 // If there are more than two consecutive empty lines, squash them into one.
 export const squashLinebreaks = (string: string): string => {
   return string.replace(/\n([^\S\n]*\n){2,}/g, '\n\n');
